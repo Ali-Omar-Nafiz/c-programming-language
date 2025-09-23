31261f82e5e80fccad2fc3dd3ae3f862760a934d
Ali Omar Nafiz <aliomarnafizbd@gmail.com>
2025-09-24 01:31:07 +0600

    update

--- Template/My Template/source.cpp
+++ Template/My Template/source.cpp
@@ -1,38 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
+const int M = 1e5, M2 = 2e5, OO = 1e9 + 7, MOD = 998244353;
+const double PI = 2 * acos(0.0);
+
 #define testcase int tc; cin >> tc; for (int i = 1; i <= tc; i++)
 #define nline "\n"
 #define all(v) v.begin(), v.end()
 // #define int long long
-using ll = long long;
-const int M = 1e5, M2 = 2e5, OO = 1e9 + 7, MOD = 998244353;
-const double PI = 2 * acos(0.0);
+
 void io() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
 }
 long long ceil(long long a, long long b) {
     return (a + b - 1) / b;
 }
 int gcd(int a, int b) {
-    int res = min(a, b);
-    while (res > 1) {
-        if (a % res == 0 && b % res == 0)
-            break;
-        res--;
-    }
-    return res;
+    return b == 0 ? a : gcd(b, a % b);
 }
 int lcm(int a, int b) {
     return (a / gcd(a, b)) * b;
 }
-void solve() {}
 
+void solve() {}
 signed main() {
     io();
       testcase {
