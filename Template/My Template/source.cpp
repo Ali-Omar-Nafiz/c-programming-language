1d369b15aae1af5af91f8f88d17c11b1ba649efa
Ali Omar Nafiz <aliomarnafizbd@gmail.com>
2025-10-07 20:58:59 +0600

    modify

--- Template/My Template/source.cpp
+++ Template/My Template/source.cpp
@@ -1,9 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
+using ull = unsigned long long;
 
-const int M = 1e5, M2 = 2e5, OO = 1e9 + 7, MOD = 998244353;
+const int M = 1e5;
+const int M2 = 2e5;
+const int OO = 1e9 + 7;
+const ll MOD = 998244353;
 const double PI = 2 * acos(0.0);
 
 #define testcase int tc; cin >> tc; for (int i = 1; i <= tc; i++)
@@ -20,41 +24,42 @@ void io() {
     // #endif
 }
 
-long long ceil(long long a, long long b) {
+ll ceilDiv(ll a, ll b) {
     return (a + b - 1) / b;
 }
 
-int gcd(int a, int b) {
+ll gcd(ll a, ll b) {
     return b == 0 ? a : gcd(b, a % b);
 }
 
-int lcm(int a, int b) {
+ll lcm(ll a, ll b) {
     return (a / gcd(a, b)) * b;
 }
 
-int stringToInt(string s) {
-    int i = 0, ret = 0;
+ll stringToLL(const string& s) {
+    ll i = 0, ret = 0;
     bool neg = false;
     if (s[0] == '-') {
         neg = true;
         i = 1;
     }
-    for (; i < (int)s.size(); i++) {
+    for (; i < (ll)s.size(); i++) {
         ret = ret * 10 + (s[i] - '0');
     }
     return neg ? -ret : ret;
 }
 
-int countDigits(int n) {
+int countDigits(ll n) {
     n = abs(n);
     if (n == 0) return 1;
     return (int)log10(n) + 1;
 }
 
 void solve() {}
-signed main() {
+
+int main() {
     io();
-      // testcase 
+    testcase
         solve();
     return 0;
 }
