221a6ebb4e31005da5cae0cce6434c8577d07b2c
Ali-Omar-Nafiz <aliomarnafizbd@gmail.com>
2026-05-17 21:10:37 +0600

    add

--- Template/My Template/source.cpp
+++ Template/My Template/source.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 using ll = long long;
+
+template <typename T, typename Comp = less < T > > 
+using  ordered_set =  tree <T, null_type, Comp, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
 #define endl '\n'
@@ -18,6 +25,7 @@ signed main() {
     int t = 1;
     cin >> t;
     for (int i = 1; i <= t; i++) {
+        // cout << "#TestCase: " << i << endl;
         solve();
     }
     return 0;
